0fed6a1436c3398957712137462f1c233ac18a4c
redknot <redknotmiaoyuqiao@gmail.com>
2020-02-12 07:17:06 +0800

    t

--- EyerVideoWand/EyerVideoProcessTest/Test.cpp
+++ EyerVideoWand/EyerVideoProcessTest/Test.cpp
@@ -6,13 +6,13 @@
 
 // #include "EyerVPAudioTrackTest.hpp"
 
-#include "EyerVPReadImageTest.h"
-#include "EyerVPAudioResTest.hpp"
+// #include "EyerVPReadImageTest.h"
+// #include "EyerVPAudioResTest.hpp"
 #include "EyerVPVideoText.h"
 
 int main(int argc,char **argv)
 {
-    RedLog("This is Test\n");
+    EyerLog("This is Test\n");
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
