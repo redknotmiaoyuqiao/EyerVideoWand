41bf1d7333cf3c15ac114bce34fa4fa4680cec36
redknot <redknotmiaoyuqiao@gmail.com>
2020-02-12 19:37:01 +0800

    test

--- EyerVideoWand/EyerVideoProcessTest/EyerVPVideoText.h
+++ EyerVideoWand/EyerVideoProcessTest/EyerVPVideoText.h
@@ -8,6 +8,8 @@
 #include <gtest/gtest.h>
 #include "EyerVideoProcess/EyerVideoProcess.hpp"
 
+#include "EyerGL/EyerGL.hpp"
+
 TEST(EyerVideoWand, Video){
     Eyer::EyerAVWriter writer("/home/redknot/Videos/number.mp4");
 
@@ -26,7 +28,61 @@ TEST(EyerVideoWand, Video){
 }
 
 TEST(GenVideo, GenVideo){
+    int width = 1280;
+    int height = 720;
 
+    Eyer::EyerAVWriter writer("/home/redknot/Videos/number_time.mp4");
+    writer.Open();
+
+    Eyer::EyerAVEncoder encoder;
+    Eyer::EncoderParam param;
+    param.codecId = Eyer::CodecId::CODEC_ID_H264;
+    param.width = width;
+    param.height = height;
+    encoder.Init(&param);
+
+    int streamId = writer.AddStream(&encoder);
+
+
+
+    Eyer::EyerGLWindow windows("GL Canvas", width, height);
+    windows.Open();
+    windows.SetBGColor(1.0, 1.0, 1.0, 1.0);
+
+    Eyer::EyerGLFrameBuffer frameBuffer;
+
+    Eyer::EyerGLTextDraw textDraw;
+    // textDraw.SetText("abcdefghijklmnopqrstuvwxyz");
+    textDraw.SetText("Redknot Miaomiao ABC GL gg");
+    textDraw.SetColor(0.0, 1.0, 0.0);
+    textDraw.SetSize(100);
+
+    textDraw.SetPos(width / 2, height / 2);
+
+    frameBuffer.AddComponent(&textDraw);
+
+    for(int i=0;i<60 * 25;i++){
+        EyerLog("Frame Id:%d\n", i);
+
+        windows.Clear();
+
+        Eyer::EyerString timeStr = Eyer::EyerString::Number(i / 25) + "~~~" + Eyer::EyerString::Number(i % 25);
+        textDraw.SetText(timeStr);
+        textDraw.SetColor(1.0, 0.0, 0.0);
+        textDraw.Viewport(width, height);
+        frameBuffer.Draw();
+
+        unsigned char * rgbData = (unsigned char * )malloc(width * height * 3);
+        frameBuffer.ReadPixel(0, 0, width, height, rgbData);
+        free(rgbData);
+
+        windows.Loop();
+    }
+
+
+    windows.Close();
+
+    writer.Close();
 }
 
 #endif //EYE_VIDEO_WAND_EYERVPVIDEOTEXT_H
